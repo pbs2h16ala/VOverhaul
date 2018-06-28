9632f3f8d09ed734a39121c756fa1bfb02e01e37
pbs2h16ala <stephanl.1995@web.de>
2018-06-28 17:13:28 +0200

    Add files via upload
    new classses

---
+++ MainClass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GTA;
+using GTA.Native;
+using GTA.Math;
+using NativeUI;
+
+namespace VampireOverhaul
+
+{
+    /*
+    *   Main App
+    */
+    public class MainClass : Script
+    {
+
+        private bool isModActive;
+        private ModMenu modMenu;
+
+        public MainClass()
+        {
+            Tick += OnTick;
+            KeyDown += OnKeyDown;
+
+            this.isModActive = false;
+            this.modMenu = new ModMenu();
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            if (!this.isModActive)
+            {
+                return;
+            }
+
+            this.modMenu.OnTick();
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F10)
+            {
+                this.ToggleMod();
+            }
+
+            if (this.isModActive)
+            {
+                this.modMenu.OnKeyDown(e);
+
+            }
+        }
+
+        private void ToggleMod()
+        {
+            if (this.isModActive)
+            {
+                this.isModActive = false;
+                UI.ShowSubtitle("Vampire Mod - disabled");
+            }
+            else
+            {
+                this.isModActive = true;
+                UI.ShowSubtitle("Vampire Mod - enabled");
+            }
+        }
+    }
+
+}
---
+++ ModMenu.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GTA;
+using GTA.Native;
+using GTA.Math;
+using NativeUI;
+using VampireOverhaul.data;
+
+namespace VampireOverhaul
+{
+    class ModMenu
+    {
+        MenuPool menuPool;
+        UIMenu modMenu_main;
+        UIMenu modMenu_powerMode;
+        AbilityBatFlash abFlash;
+
+        public ModMenu()
+        {
+            this.InitModMenu();
+            abFlash = new AbilityBatFlash();
+        }
+        
+        private void InitModMenu()
+        {
+            this.menuPool = new MenuPool();
+
+            this.modMenu_main = new UIMenu("Vampire Overhaul", "Choose your settings");
+            this.menuPool.Add(modMenu_main);
+
+            this.modMenu_powerMode = menuPool.AddSubMenu(modMenu_main, "Power Mode");
+        }
+
+
+        public void OnTick()
+        {
+            this.ProcessMenusIfNeeded();
+        }
+
+        private void ProcessMenusIfNeeded()
+        {
+            if (this.modMenu_main.Visible)
+            {
+                this.menuPool.ProcessMenus();
+            }
+        }
+
+        public void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.L)
+            {
+                abFlash.PerformAbilityAction();
+            }
+        }
+
+        public void ToggleMainMenu()
+        {
+            this.modMenu_main.Visible = !this.modMenu_main.Visible;
+        }
+        
+    }
+}
